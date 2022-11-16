47119be4d10d2e472386a3ff5a726b6e95df73a2
DeemAlsh <83668026+DeemAlsh@users.noreply.github.com>
2022-11-16 13:00:48 +0300

    Add files via upload

---
+++ hotel reservation/src/model/Room.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Room implements IRoom {
+    private final String roomNumber;
+    private final Double roomPrice;
+    private final RoomType enumeration;
+
+    public Room(String roomNumber, Double roomPrice, RoomType enumeration) {
+        this.roomNumber = roomNumber;
+        this.roomPrice = roomPrice;
+        this.enumeration = enumeration;
+    }
+
+    @Override
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return roomPrice;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return enumeration;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Room number: " + getRoomNumber() + ", Room price: " + getRoomPrice() + ", Room type; " + getRoomType();
+    }
+}
